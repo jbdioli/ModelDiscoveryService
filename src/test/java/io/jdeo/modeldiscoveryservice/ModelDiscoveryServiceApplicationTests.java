c3a9ca40435704b5a3f40987e3fcefe9b6413836
jbdioli <jdeoworld@gmail.com>
2022-05-27 10:37:06 +0200

    add

---
+++ src/test/java/io/jdeo/modeldiscoveryservice/ModelDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jdeo.modeldiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
